28b0d2a49ffa173d6346720f7f6a6587f49298fa
ruudschouten <ruud.schouten@outlook.com>
2020-03-14 00:44:40 +0100

    Updated with new Book constructor

--- app/src/main/java/com/ruurd/peruse/ui/fragments/viewmodels/LibraryViewModel.kt
+++ app/src/main/java/com/ruurd/peruse/ui/fragments/viewmodels/LibraryViewModel.kt
@@ -3,6 +3,7 @@ package com.ruurd.peruse.ui.fragments.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ruurd.peruse.models.Author
 import com.ruurd.peruse.models.Book
 import com.ruurd.peruse.models.Chapter
 
@@ -19,7 +20,14 @@ class LibraryViewModel : ViewModel() {
 
     private fun loadBooks(): MutableLiveData<ArrayList<Book>> {
         // Mocking :)
-        val book = Book("His Dark Materials", mutableListOf(Chapter("1", 21), Chapter("2", 18)))
-        return MutableLiveData(arrayListOf(book, book, book, book, book, book))
+        val book = Book(
+            "His Dark Materials",
+            mutableListOf(
+                Chapter("1", 21),
+                Chapter("2", 18)
+            ),
+            Author("Philip Pullman")
+        )
+        return MutableLiveData(arrayListOf(book))
     }
 }
