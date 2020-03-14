ff57d7b71673440e7360992fe6dc7e80c8e4d634
ruudschouten <ruud.schouten@outlook.com>
2020-03-14 20:37:53 +0100

    Can now insert a new book by string values Now also uses Coroutines to insert items to the database

--- app/src/main/java/com/ruurd/peruse/data/repository/AppRepository.kt
+++ app/src/main/java/com/ruurd/peruse/data/repository/AppRepository.kt
@@ -1,36 +1,66 @@
 package com.ruurd.peruse.data.repository
 
+import android.content.Context
 import com.ruurd.peruse.data.AppDatabase
+import com.ruurd.peruse.models.Author
 import com.ruurd.peruse.models.Book
+import com.ruurd.peruse.models.Series
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class AppRepository(db: AppDatabase) {
-    private val bookRepository = BookRepository(db.bookDao())
-    private val authorRepository = AuthorRepository(db.authorDao())
-    private val seriesRepository = SeriesRepository(db.seriesDao())
-    private val chapterRepository = ChapterRepository(db.chapterDao())
+class AppRepository(context: Context) : CoroutineScope {
+    private val bookRepository = BookRepository(context)
+    private val authorRepository = AuthorRepository(context)
+    private val seriesRepository = SeriesRepository(context)
+    private val chapterRepository = ChapterRepository(context)
 
-    fun insert(book: Book) : Long {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Default
+
+    fun insert(bookTitle: String, authorName: String) = launch {
+        val author = Author(authorName)
+        insert(Book(bookTitle, mutableListOf(), author))
+    }
+
+    fun insert(
+        bookTitle: String,
+        authorName: String,
+        seriesName: String,
+        seriesEntry: Float
+    ) = launch {
+        val author = Author(authorName)
+        val series = Series(seriesName)
+        insert(Book(bookTitle, mutableListOf(), author, series, seriesEntry))
+    }
+
+    fun insert(book: Book): Long {
         // Create a POJO from book.
         val bookPojo = book.toPojo()
-        // Check if books author exists.
-        val author = authorRepository.insertOrGetByName(book.author.name)
-        bookPojo.bookAuthorId = author.authorId
+        var bookId = 0L
 
-        // Check if book is in a series.
-        if (book.isInSeries()) {
-            val series = seriesRepository.insertOrGetByName(book.series!!.name)
-            bookPojo.bookSeriesId = series.seriesId
-            bookPojo.seriesEntry = book.seriesEntry
-        }
+        launch {
+            // Check if books author exists.
+            val author = authorRepository.insertOrGetByName(book.author.name)
+            bookPojo.bookAuthorId = author.authorId
 
-        val bookId = bookRepository.insert(bookPojo)
+            // Check if book is in a series.
+            if (book.isInSeries()) {
+                val series = seriesRepository.insertOrGetByName(book.series!!.name)
+                bookPojo.bookSeriesId = series.seriesId
+                bookPojo.seriesEntry = book.seriesEntry
+            }
 
-        // Check if any chapters have been added.
-        if (book.chapters.any()) {
-            book.chapters.forEach {
-                val chapterPojo = it.toPojo()
-                chapterPojo.bookId = bookId
-                chapterRepository.insert(chapterPojo)
+            bookId = bookRepository.insert(bookPojo)
+
+            // Check if any chapters have been added.
+            if (book.chapters.any()) {
+                book.chapters.forEach {
+                    val chapterPojo = it.toPojo()
+                    chapterPojo.bookId = bookId
+                    chapterRepository.insert(chapterPojo)
+                }
             }
         }
 
