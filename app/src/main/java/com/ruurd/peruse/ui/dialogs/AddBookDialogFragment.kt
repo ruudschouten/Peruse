e1d5a0fc5913b57d2443c42573ac839e8af3d5e2
ruudschouten <ruud.schouten@outlook.com>
2020-03-14 20:32:47 +0100

    Reworked book dialog
    Now only uses EditTexts
    Now has a Checkbox to determine if a book is in a series

--- app/src/main/java/com/ruurd/peruse/ui/dialogs/AddBookDialogFragment.kt
+++ app/src/main/java/com/ruurd/peruse/ui/dialogs/AddBookDialogFragment.kt
@@ -2,14 +2,15 @@ package com.ruurd.peruse.ui.dialogs
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
-import android.widget.Spinner
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
-import com.google.android.material.snackbar.Snackbar
 import com.ruurd.peruse.R
-import java.lang.IllegalStateException
+import com.ruurd.peruse.data.repository.AppRepository
 
 class AddBookDialogFragment : DialogFragment() {
 
@@ -22,27 +23,49 @@ class AddBookDialogFragment : DialogFragment() {
         val inflater = requireActivity().layoutInflater
         val root = inflater.inflate(R.layout.dialog_new_book, null)
 
-        // Getting views.
+        val appRepo = AppRepository(context!!)
 
+        // Getting views.
         val bookTitleTextEdit = root.findViewById<EditText>(R.id.dialog_book_title)
-        val seriesSpinner = root.findViewById<Spinner>(R.id.dialog_book_series_spinner)
-        val writerSpinner = root.findViewById<Spinner>(R.id.dialog_book_author_spinner)
+        val bookAuthorTextEdit = root.findViewById<EditText>(R.id.dialog_book_author)
+        val bookSeriesCheckbox = root.findViewById<CheckBox>(R.id.dialog_book_series_checkbox)
+        val bookSeriesContainer =
+            root.findViewById<ConstraintLayout>(R.id.dialog_book_series_container)
+        val bookSeriesTextEdit = root.findViewById<EditText>(R.id.dialog_book_series)
+        val bookSeriesEntryTextEdit = root.findViewById<EditText>(R.id.dialog_book_series_entry)
         val cancelButton = root.findViewById<Button>(R.id.dialog_book_btn_cancel)
         val addButton = root.findViewById<Button>(R.id.dialog_book_btn_add)
 
-        // Set values for the spinners.
-
         // Setting events.
+        bookSeriesCheckbox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                bookSeriesContainer.visibility = View.VISIBLE
+            } else {
+                bookSeriesContainer.visibility = View.GONE
+            }
+        }
+
         cancelButton.setOnClickListener {
             dialog?.cancel()
         }
 
         addButton.setOnClickListener {
-            Snackbar.make(root, String.format("Adding %s", bookTitleTextEdit.text), 500).show()
+            // TODO: Add null checks or disable button until all required fields have values
+            val title = bookTitleTextEdit.text.toString()
+            val author = bookAuthorTextEdit.text.toString()
+
+            if (bookSeriesCheckbox.isChecked) {
+                val series = bookSeriesTextEdit.text.toString()
+                val entry = bookSeriesEntryTextEdit.text.toString().toFloat()
+
+                appRepo.insert(title, author, series, entry)
+            } else {
+                appRepo.insert(title, author)
+            }
+            dialog?.cancel()
         }
 
         builder.setView(root)
         return builder.create()
     }
-
 }
