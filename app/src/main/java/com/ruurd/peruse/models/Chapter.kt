bbe55d6250f17e56c5014be2bdbd128ce2e34be7
ruudschouten <ruud.schouten@outlook.com>
2020-03-20 16:49:14 +0100

    Added function which returns the duration of the read chapter in a formatted way

--- app/src/main/java/com/ruurd/peruse/models/Chapter.kt
+++ app/src/main/java/com/ruurd/peruse/models/Chapter.kt
@@ -1,6 +1,9 @@
 package com.ruurd.peruse.models
 
+import android.content.Context
+import com.ruurd.peruse.R
 import com.ruurd.peruse.data.pojo.ChapterPOJO
+import java.util.concurrent.TimeUnit
 
 data class Chapter(
     val title: String,
@@ -12,6 +15,19 @@ data class Chapter(
         title: String,
         pages: Int
     ) : this(title, pages, 0L)
+
+    fun getFormattedTime(context: Context): String {
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
+
+        return String.format(
+            context.getString(
+                R.string.time_format,
+                minutes.toString(),
+                seconds.toString()
+            )
+        )
+    }
 
     override fun toPojo(): ChapterPOJO {
         return ChapterPOJO(title, pages, duration)
