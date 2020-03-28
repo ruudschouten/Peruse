c2507481219462bb2e2146291d1f1f985f1a112d
ruudschouten <ruud.schouten@outlook.com>
2020-03-28 16:00:20 +0100

    Moved string to strings.xml Removed status text, since a new chapter is always read

--- app/src/main/java/com/ruurd/peruse/ui/adapters/BookChapterViewHolder.kt
+++ app/src/main/java/com/ruurd/peruse/ui/adapters/BookChapterViewHolder.kt
@@ -1,27 +1,18 @@
 package com.ruurd.peruse.ui.adapters
 
 import android.view.View
-import android.view.View.GONE
-import androidx.navigation.NavController
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import com.ruurd.peruse.R
 import com.ruurd.peruse.data.pojo.ChapterPOJO
 import com.ruurd.peruse.models.Chapter
 import com.ruurd.peruse.ui.adapters.abstracts.POJOViewHolder
 import kotlinx.android.synthetic.main.recycler_book_chapter.view.*
 
-class BookChapterViewHolder(v: View)
-    : POJOViewHolder<ChapterPOJO, Chapter>(v) {
+class BookChapterViewHolder(v: View) : POJOViewHolder<ChapterPOJO, Chapter>(v) {
 
     override fun setupViews() {
-        if (model.duration > 0) {
-            view.book_chapter_recycler_status.text = "✔"
-        } else {
-            view.book_chapter_recycler_status.visibility = GONE
-        }
-
         view.book_chapter_recycler_title.text = model.title
-        view.book_chapter_recycler_pages.text = model.pages.toString()
+        view.book_chapter_recycler_pages.text = view.context.getString(R.string.book_chapters_pages, model.pages.toString())
         view.book_chapter_recycler_time.text = model.getFormattedTime(view.context)
     }
 
