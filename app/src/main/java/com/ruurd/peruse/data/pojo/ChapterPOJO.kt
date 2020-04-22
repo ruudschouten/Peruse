3b6b04eff6b4b5c69371a4ee0117f082708bed5b
ruudschouten <ruud.schouten@outlook.com>
2020-04-22 16:31:29 +0200

    Chapter now keeps track of page start, end and the date it was read Made progress on #2

--- app/src/main/java/com/ruurd/peruse/data/pojo/ChapterPOJO.kt
+++ app/src/main/java/com/ruurd/peruse/data/pojo/ChapterPOJO.kt
@@ -7,34 +7,34 @@ import com.ruurd.peruse.models.Chapter
 @Entity(tableName = "chapters")
 data class ChapterPOJO(
     val title: String,
+    var start: Int,
+    var end: Int,
     var pages: Int,
     var duration: Long,
+    var date: Long,
     var bookId: Long
 ) : PojoToModel<Chapter> {
     constructor(
         title: String,
-        pages: Int
-    ) : this(title, pages, 0, 0)
-
-    constructor(
-        title: String,
-        pages: Int,
-        duration: Long
-    ) : this(title, pages, duration, 0)
-
-    constructor(
-        id: Long,
-        title: String,
-        pages: Int,
-        duration: Long
-    ) : this(title, pages, duration, 0) {
-        chapterId = id
+        start: Int,
+        end: Int
+    ) : this(title, start, end, 0, 0L, 0L, 0) {
+        pages = (end - start) + 2
     }
+
+    constructor(
+        title: String,
+        start: Int,
+        end: Int,
+        pages: Int,
+        duration: Long,
+        date: Long
+    ) : this(title, start, end, pages, duration, date, 0)
 
     @PrimaryKey(autoGenerate = true)
     var chapterId: Long = 0
 
     override fun toModel(): Chapter {
-        return Chapter(chapterId, title, pages, duration)
+        return Chapter(chapterId, title, start, end, pages, duration, date)
     }
 }
--- app/src/main/java/com/ruurd/peruse/models/Chapter.kt
+++ app/src/main/java/com/ruurd/peruse/models/Chapter.kt
@@ -7,32 +7,51 @@ import com.ruurd.peruse.util.TimeUtil
 data class Chapter(
     var id: Long,
     var title: String,
+    var start: Int,
+    var end: Int,
     var pages: Int,
-    var duration: Long
+    var duration: Long,
+    var date: Long
 ) : ModelToPojo<ChapterPOJO> {
 
     constructor(
         title: String,
-        pages: Int
-    ) : this(0L, title, pages, 0L)
+        start: Int,
+        end: Int
+    ) : this(0L, title, start, end, 0, 0L, 0L) {
+        pages = (end - start) + 2
+    }
 
     constructor(
         title: String,
-        pages: Int,
-        duration: Long
-    ) : this(0L, title, pages, duration)
+        start: Int,
+        end: Int,
+        date: Long
+    ) : this(0L, title, start, end, 0, 0L, date) {
+        pages = (end - start) + 2
+    }
 
-    constructor() : this("", 0)
+    constructor(
+        title: String,
+        start: Int,
+        end: Int,
+        duration: Long,
+        date: Long
+    ) : this(0L, title, start, end, 0, duration, date) {
+        pages = (end - start) + 2
+    }
+
+    constructor() : this("", 0, 0)
 
     fun getFormattedTime(context: Context): String {
         return TimeUtil.toTime(duration).format(context)
     }
 
     override fun toPojo(): ChapterPOJO {
-        return ChapterPOJO(id, title, pages, duration)
+        return ChapterPOJO(title, start, end, pages, duration, date)
     }
 
     fun toPojo(bookId: Long) : ChapterPOJO {
-        return ChapterPOJO(id, title, pages, duration).also { it.bookId = bookId }
+        return toPojo().also { it.bookId = bookId }
     }
 }
