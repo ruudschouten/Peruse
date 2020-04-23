c3485e58ef96cd01f57ccc12c3030647e115348a
ruudschouten <ruud.schouten@outlook.com>
2020-04-23 14:15:55 +0200

    Chapters now show what date they're read in Chapters now show their start and end page Closes #2

--- app/src/main/java/com/ruurd/peruse/ui/adapters/BookChapterViewHolder.kt
+++ app/src/main/java/com/ruurd/peruse/ui/adapters/BookChapterViewHolder.kt
@@ -6,13 +6,15 @@ import com.ruurd.peruse.R
 import com.ruurd.peruse.data.pojo.ChapterPOJO
 import com.ruurd.peruse.models.Chapter
 import com.ruurd.peruse.ui.adapters.abstracts.POJOViewHolder
+import com.ruurd.peruse.util.DateUtil
 import kotlinx.android.synthetic.main.recycler_book_chapter.view.*
 
 class BookChapterViewHolder(v: View) : POJOViewHolder<ChapterPOJO, Chapter>(v) {
 
     override fun setupViews() {
         view.book_chapter_recycler_title.text = model.title
-        view.book_chapter_recycler_pages.text = view.context.getString(R.string.book_chapters_pages, model.pages.toString())
+        view.book_chapter_recycler_date.text = DateUtil.format(model.date)
+        view.book_chapter_recycler_pages.text = view.context.getString(R.string.book_chapters_pages, model.pages, model.start, model.end)
         view.book_chapter_recycler_time.text = model.getFormattedTime(view.context)
     }
 
