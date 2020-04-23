ea1c1610e39dfac531cd5405ff4f6be9fbf6482a
ruudschouten <ruud.schouten@outlook.com>
2020-04-23 12:05:51 +0200

    Not entering a first chapter when reading now no longer enters 0 in the start field of the chapter read

--- app/src/main/java/com/ruurd/peruse/ui/adapters/ReadingChapterViewHolder.kt
+++ app/src/main/java/com/ruurd/peruse/ui/adapters/ReadingChapterViewHolder.kt
@@ -9,7 +9,10 @@ class ReadingChapterViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(chapter: Chapter) {
         view.chapter_reading_title.setText(chapter.title)
-        view.dialog_book_pages_start.setText(chapter.start.toString())
+        // Only set the value of the pages if it isn't 0. If the start is 0, it's a default Chapter.
+        if (chapter.start != 0) {
+            view.dialog_book_pages_start.setText(chapter.start.toString())
+        }
     }
 
     fun isAnyFieldEmpty(): Boolean {
@@ -30,6 +33,11 @@ class ReadingChapterViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
         val start = view.dialog_book_pages_start.text.toString().toInt()
         val end = view.dialog_book_pages_end.text.toString().toInt()
 
-        return Chapter(view.chapter_reading_title.text.toString(), start, end, System.currentTimeMillis())
+        return Chapter(
+            view.chapter_reading_title.text.toString(),
+            start,
+            end,
+            System.currentTimeMillis()
+        )
     }
 }
