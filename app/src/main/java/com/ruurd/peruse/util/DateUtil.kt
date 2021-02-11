f167ecc32d11cc40a9d769cf44c031978791715b
ruudschouten <ruud.schouten@outlook.com>
2021-02-11 10:52:07 +0100

    Formatting

--- app/src/main/java/com/ruurd/peruse/util/DateUtil.kt
+++ app/src/main/java/com/ruurd/peruse/util/DateUtil.kt
@@ -5,14 +5,14 @@ import java.util.*
 
 object DateUtil {
 
-    fun format(duration: Long) : String {
+    fun format(duration: Long): String {
         val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = duration
         return formatter.format(calendar.time)
     }
 
-    fun formatWithTime(duration: Long) : String {
+    fun formatWithTime(duration: Long): String {
         val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = duration
