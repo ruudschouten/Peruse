dee5aa54f30eaa26a3219a5d481c31d250173884
ruudschouten <ruud.schouten@outlook.com>
2021-02-11 10:53:30 +0100

    Added Settings and BackupManager. Settings right now only has the possibilities to backup and restore a database. BackupManager handles the backing up, picking the database file, and restoring it.

--- app/src/main/java/com/ruurd/peruse/ui/activities/SettingsActivity.kt
+++ app/src/main/java/com/ruurd/peruse/ui/activities/SettingsActivity.kt
@@ -1,14 +1,25 @@
 package com.ruurd.peruse.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.ruurd.peruse.R
+import com.ruurd.peruse.data.DataUtil
+import com.ruurd.peruse.data.backup.BackupManager
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_settings)
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
@@ -18,9 +29,45 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            DataUtil.restoreResultCode -> {
+                runBlocking {
+                    BackupManager.restoreBackup(this@SettingsActivity, data)
+                }
+            }
+        }
+    }
+
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val backupButton = findPreference<Preference>(getString(R.string.backup_data_key))!!
+            backupButton.setOnPreferenceClickListener {
+                runBlocking {
+                    BackupManager.createBackup(requireContext())
+                    return@runBlocking true
+                }
+            }
+
+            val restoreButton = findPreference<Preference>(getString(R.string.restore_data_key))!!
+            restoreButton.setOnPreferenceClickListener {
+                BackupManager.pickDatabaseFile(requireActivity())
+                true
+            }
         }
     }
 }
